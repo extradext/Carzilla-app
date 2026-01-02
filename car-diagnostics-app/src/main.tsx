15b3419d6e6a0eddc1ffd4769a30eef2e9235431
extradext <36736464+extradext@users.noreply.github.com>
2026-01-01 22:38:18 -0600

    Update main.tsx

--- car-diagnostics-app/src/main.tsx
+++ car-diagnostics-app/src/main.tsx
@@ -9,8 +9,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./ui/App";
-import "./styles.css";
 
+import "./styles.css?forceReload=1";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
